e2c45c4b110dcec39e25dfdfdab52bb3fbc397b9
guyfox2001 <64916542+guyfox2001@users.noreply.github.com>
2021-12-21 22:33:39 +0300

    Update anounsment.php

--- anounsment.php
+++ anounsment.php
@@ -8,7 +8,7 @@ $items = $pdo->query($sql);
 foreach ($items as $item): ?>
 <div class="annsmnt">
     <p><?php print_r($item['name_item'])?></p>
-    <img src="pictures/<?php print_r($item['picture'])?>" alt="pictures/exmpl.jpg">
+    <img src="pictures/<?php print_r($item['picture'])?>" alt="no pic">
     <p>Price: <?php print_r($item['price'])?>$</p>
 
     <button id="show_more">Show More</button>
